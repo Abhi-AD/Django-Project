6a41563d185e18d6205c45bf20c1d96ca0e16eb6
Abhi-AD <dangiabhi332@gmail.com>
2024-11-05 21:49:25 +0545

    feat:vendor details

--- Ecom/apps/essence/urls.py
+++ Ecom/apps/essence/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("products/", views.product_list_view, name="product-list"),
     path("category/<cid>/", views.category_product_list, name="category-product-list"),
     path("vendors/", views.vendor_list_view, name="vendor-list"),
+    path("vendors/<vid>", views.vendor_details_view, name="vendor-detail"),
 ]
